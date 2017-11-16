1c01de230751dbe7238eb8d0eeb09af422342aeb
Denham Preen <denhampreen@gmail.com>
2017-11-16 20:53:50 +0200

    basic mvp, missing a few slides

---
+++ js/slideConfig.js
@@ -0,0 +1,163 @@
+var slides = [
+	
+	//Chapter 1
+	
+	[],
+	
+	//Chapter 2
+	
+	["2_stressAndStrain/2.1_M1.2.swf", 		
+	 "2_stressAndStrain/2.2_M2.1.swf", 
+	 "2_stressAndStrain/2.3_M2.2.swf", 
+	 "2_stressAndStrain/2.4_M2.3.swf", 
+	 "2_stressAndStrain/2.5_M2.4.swf", 
+	 "2_stressAndStrain/2.6_M1.1.swf", 
+	 "2_stressAndStrain/2.7_M3.1.swf", 
+	 "2_stressAndStrain/2.8_M3.2.swf", 
+	 "2_stressAndStrain/2.9_M3.3.swf", 
+	 "2_stressAndStrain/2.10_M1.3.swf", 
+	 "2_stressAndStrain/2.11_M1.4.swf", 
+	 "2_stressAndStrain/2.12_M1.5.swf", 
+	 "2_stressAndStrain/2.13_M1.6.swf",
+	 "2_stressAndStrain/2.14_M1.7.swf",
+	 "2_stressAndStrain/2.15_M1.8.swf",
+	 "2_stressAndStrain/2.16_M1.9.swf",
+	 "2_stressAndStrain/2.17_M1.10.swf",
+	 "2_stressAndStrain/2.18_M2.5.swf"],
+	
+	//Chapter 3
+	
+	 ["3_stressTransform/3.1_M1.11.swf",
+	  "3_stressTransform/3.2_M1.12.swf", 
+	  "3_stressTransform/3.3_M12.1.swf", 
+	  "3_stressTransform/3.4_M12.2.swf", 
+	  "3_stressTransform/3.5_M12.3.swf",
+	  "3_stressTransform/3.6_M12.4.swf",
+	  "3_stressTransform/3.7_M12.5.swf",
+	  "3_stressTransform/3.8_M12.6.swf",
+	  "3_stressTransform/3.9_M12.7.swf",
+	  "3_stressTransform/3.10_M12.8.swf",
+	  "3_stressTransform/3.11_M12.9.swf",
+	  "3_stressTransform/3.12_M12.10.swf",
+	  "3_stressTransform/3.13_M12.11.swf",
+	  "3_stressTransform/3.14_M12.12.swf",
+	  "3_stressTransform/3.15_M12.13.swf",
+	  "3_stressTransform/3.16_M12.14.swf",
+	  "3_stressTransform/3.17_M12.15.swf",
+	  "3_stressTransform/3.18_M12.16.swf",
+	  "3_stressTransform/3.19_M12.17.swf",
+	  "3_stressTransform/3.20_M12.18.swf",
+	  "3_stressTransform/3.21_M12.19.swf"],
+	
+	//Chapter 4
+	
+	["4_axial/4.1_M5.1.swf",
+	  "4_axial/4.2_M5.2.swf",
+	  "4_axial/4.3_M5.3.swf",
+	  "4_axial/4.4_M5.4.swf",
+	  "4_axial/4.5_M5.5.swf",
+	  "4_axial/4.6_M5.6.swf",
+	  "4_axial/4.7_M5.7.swf",
+	  "4_axial/4.8_M5.8.swf",
+	  "4_axial/4.9_M5.9.swf",
+	  "4_axial/4.10_M5.10.swf",
+	  "4_axial/4.11_M5.11.swf",
+	  "4_axial/4.12_M5.12.swf",
+	  "4_axial/4.13_M5.13.swf",
+	  "4_axial/4.14_M5.14.swf",
+	  "4_axial/4.15_M5.15.swf"],
+	
+	//Chapter 5
+	
+	["5_torsion/5.1_M6.1.swf",
+	  "5_torsion/5.2_M6.2.swf",
+	  "5_torsion/5.3_M6.3.swf",
+	  "5_torsion/5.4_M6.4.swf",
+	  "5_torsion/5.5_M6.5.swf",
+	  "5_torsion/5.6_M6.6.swf",
+	  "5_torsion/5.7_M6.7.swf",
+	  "5_torsion/5.8_M6.8.swf",
+	  "5_torsion/5.9_M6.9.swf",
+	  "5_torsion/5.10_M6.10.swf",
+	  "5_torsion/5.11_M6.11.swf",
+	  "5_torsion/5.12_M6.12.swf",
+	  "5_torsion/5.13_M6.13.swf",
+	  "5_torsion/5.14_M6.14.swf",
+	  "5_torsion/5.15_M6.15.swf",
+	  "5_torsion/5.16_M6.16.swf",
+	  "5_torsion/5.17_M6.17.swf",
+	  "5_torsion/5.18_M6.19.swf",
+	  "5_torsion/5.19_M6.20.swf",
+	  "5_torsion/5.20_M6.21.swf",
+	  "5_torsion/5.21_M6.22.swf",
+	  "5_torsion/5.22_M6.23.swf",
+	  "5_torsion/5.23_M6.24.swf",
+	  "5_torsion/5.24_M15.1.swf"],
+	
+	//Chapter 6
+	
+	["6_beamEquilibrium/6.1_M7.1.swf",
+	  "6_beamEquilibrium/6.2_shearForceFollowingTheRules.swf",
+	  "6_beamEquilibrium/6.3_shearForceStepByStep.swf"],
+	
+	//Chapter 7
+	
+	 ["7_bendingStress/7.1_M8.1.swf",
+	  "7_bendingStress/7.2_M8.2.swf",
+	  "7_bendingStress/7.3_M8.3.swf",
+	  "7_bendingStress/7.4_M8.4.swf",
+	  "7_bendingStress/7.5_M8.5.swf",
+	  "7_bendingStress/7.6_M8.6.swf",
+	  "7_bendingStress/7.7_M8.7.swf",
+	  "7_bendingStress/7.8_M8.8.swf",
+	  "7_bendingStress/7.9_M8.9.swf",
+	  "7_bendingStress/7.10_M8.10.swf",
+	  "7_bendingStress/7.11_M8.11.swf",
+	  "7_bendingStress/7.12_M8.12.swf",
+	  "7_bendingStress/7.13_M8.13.swf",
+	  "7_bendingStress/7.14_M8.14.swf",
+	  "7_bendingStress/7.15_M8.15.swf",
+	  "7_bendingStress/7.16_M8.16.swf",
+	  "7_bendingStress/7.17_M8.17.swf",
+	  "7_bendingStress/7.28_M8.18.swf",
+	  "7_bendingStress/7.19_M8.19.swf",
+	  "7_bendingStress/7.20_M8.20.swf",
+	  "7_bendingStress/7.21_M8.21.swf",
+	  "7_bendingStress/7.22_M8.23.swf"],
+	
+	//Chapter 8
+	
+	["8_beamDeflections/8.1_M10.1.swf",
+	  "8_beamDeflections/8.2_M10.2.swf",
+	  "8_beamDeflections/8.3_M10.3.swf",
+	  "8_beamDeflections/8.4_M10.4.swf",
+	  "8_beamDeflections/8.5_M10.5.swf",
+	  "8_beamDeflections/8.6_M10.6.swf",
+	  "8_beamDeflections/8.7_M10.7.swf",
+	  "8_beamDeflections/8.8_M10.8.swf",
+	  "8_beamDeflections/8.9_M10.9.swf",
+	  "8_beamDeflections/8.10_M10.10.swf",
+	  "8_beamDeflections/8.11_M10.11.swf",
+	  "8_beamDeflections/8.12_M10.12.swf",
+	  "8_beamDeflections/8.13_M10.13.swf",
+	  "8_beamDeflections/8.14_M10.14.swf",
+	  "8_beamDeflections/8.15_M11.1.swf",
+	  "8_beamDeflections/8.16_M11.2.swf",
+	  "8_beamDeflections/8.17_M11.3.swf",
+	  "8_beamDeflections/8.18_M11.4.swf"
+	   ]
+
+	
+]; //array of arrays or chapters of slides 
+
+var courseNotes = [
+	"CourseNotes.pdf",
+	"Chapter1.pdf",
+	"Chapter2.pdf",
+	"Chapter3.pdf",
+	"Chapter4.pdf",
+	"Chapter5.pdf",
+	"Chapter6.pdf",
+	"Chapter7.pdf",
+	"Chapter8.pdf"	
+];
